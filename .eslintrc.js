bd06684b74d541f0e73a28f0104fcb01be9e5d72
over-engineer <dev@over-engineer.com>
2020-08-08 16:25:19 +0300

    Update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "indent": ["error", 4],
-        "no-underscore-dangle": "off",
-        "no-console": "off",
-    }
+    env: {
+        es6: true,
+        browser: true,
+    },
+    extends: [
+        'airbnb-base'
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        'no-underscore-dangle': 'off',
+        'no-console': 'off',
+    },
 };
